44eb854271cd0c98219883babdbb1ed1cbfc1808
KerBerStar <42485058+KerBerStar@users.noreply.github.com>
2018-08-18 10:40:24 +0400

    HTML SerVer.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/html/index.html');
+});
+
+http.listen(1337, function(){console.log('Прослушка *:1337');});
+io.on('connection', function(socket){console.log('a user connected');});
