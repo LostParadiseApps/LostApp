a82697ed293e4378916b967b5bf5480a3c500523
KerBerStar <42485058+KerBerStar@users.noreply.github.com>
2018-08-19 21:54:32 +0400

    123

--- server.js
+++ server.js
@@ -4,9 +4,24 @@ const io = require('socket.io')(http);
 
 
 app.get('/', function(req, res){
-
+  console.log(req);
+  console.log(res);
   res.sendFile(__dirname + '/public/html/index.html');
 });
 
-http.listen(80, function(){console.log('Прослушка *:1337');});
+app.get('/client.js', function(req, res){
+  console.log(req);
+  console.log(res);
+    res.sendFile(__dirname + '/public/Js/client.js');
+
+});
+app.get('/pixi.js', function(req, res){
+  console.log(req);
+  console.log(res);
+    res.sendFile(__dirname + '/public/Js/client.js');
+
+});
+
+
+http.listen(80, function(){console.log('Прослушка *:80');});
 io.on('connection', function(socket){console.log('a user connected');});
